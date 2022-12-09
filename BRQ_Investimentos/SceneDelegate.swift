aa744111b033c049fefc3a22c09ac2af04d84636
Al3x1sCS <prog.alexis.cs@protonmail.com>
2022-12-09 12:34:18 -0300

    feat: now the SceneDelegate has the correct responsibility.

--- BRQ_Investimentos/SceneDelegate.swift
+++ BRQ_Investimentos/SceneDelegate.swift
@@ -13,10 +13,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     //TODO: Usar o willConnectTo para trabalhar em view code e linkar com o coordinator para dividir a responsabilidade de navegar entre as telas
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let winScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: winScene)
+        window?.makeKeyAndVisible()
+        
+        let navigationController = UINavigationController()
+        window?.rootViewController = navigationController
+        
+        let coordinator = HomeCoordinator(navigationController: navigationController)
+        coordinator.start()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
