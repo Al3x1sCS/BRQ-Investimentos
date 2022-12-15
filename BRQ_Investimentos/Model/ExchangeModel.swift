ebe91c59f9cb0b6af431183e9c0849366cda6e7a
Al3x1sCS <prog.alexis.cs@protonmail.com>
2022-12-15 12:04:11 -0300

    refactor: minor refactor.

--- BRQ_Investimentos/Model/ExchangeModel.swift
+++ BRQ_Investimentos/Model/ExchangeModel.swift
@@ -8,10 +8,20 @@
 import Foundation
 import UIKit
 
-class ExchangeModel {
+struct ExchangeModel {
+    let name: String
+    let sigla: String
+    let variation: Double
+    let buy: Double
+    let sell: Double
     let coin: Coins
     
     init(coin: Coins) {
         self.coin = coin
+        self.name = coin.name
+        self.sigla = coin.sigla ?? "USD"
+        self.variation = coin.variation
+        self.buy = coin.buy ?? 0
+        self.sell = coin.sell ?? 0
     }
 }
